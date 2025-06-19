aacd7bf041da36b761cd69405b4e5c341cb52ea5
Darshani <radkumari@std.appsc.sab.ac.lk>
2025-06-19 10:00:29 +0530

    Cnnect contact infor

---
+++ script.js
@@ -0,0 +1,9 @@
+function showContact() {
+  document.getElementById('overlay').style.display = 'block';
+  document.getElementById('contactBox').style.display = 'block';
+}
+
+function hideContact() {
+  document.getElementById('overlay').style.display = 'none';
+  document.getElementById('contactBox').style.display = 'none';
+}
